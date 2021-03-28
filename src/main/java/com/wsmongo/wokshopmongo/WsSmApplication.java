f83b754429b7f71729f898c9e56fb00d6bcc0aa0
Evaldo Martins <evaldojunior1997@hotmail.com>
2021-03-28 20:26:29 -0300

    Primeiro commit - projeto criado

---
+++ src/main/java/com/wsmongo/wokshopmongo/WsSmApplication.java
@@ -0,0 +1,13 @@
+package com.wsmongo.wokshopmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsSmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsSmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wsmongo/wokshopmongo/WsSmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wsmongo.wokshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsSmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
