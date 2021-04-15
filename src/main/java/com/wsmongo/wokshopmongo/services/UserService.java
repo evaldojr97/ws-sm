9c6ef5d55a0065ffd03b4c1a219ed5ca6542e468
Evaldo Martins <evaldojunior1997@hotmail.com>
2021-04-14 22:36:23 -0300

    Obtendo um usuario por id

--- src/main/java/com/wsmongo/wokshopmongo/services/UserService.java
+++ src/main/java/com/wsmongo/wokshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.wsmongo.wokshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.wsmongo.wokshopmongo.domain.User;
 import com.wsmongo.wokshopmongo.repository.UserRepository;
+import com.wsmongo.wokshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,10 @@ public class UserService {
 	
 	public List<User> findAll() {
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 }
